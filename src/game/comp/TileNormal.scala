7cef8a6e3418a9c7168caf0d6eee446eb33a0bc5
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-12 05:22:07 +0700

    Update TileNormal.scala

--- src/game/comp/TileNormal.scala
+++ src/game/comp/TileNormal.scala
@@ -1,11 +1,12 @@
 package game.comp
 
-class TileNormal(override val value:Int) extends Tile(value){
+class TileNormal( override val newValue:Int) extends Tile(newValue){
 this.target=this
   
   
   override def toString(): String = {
    val res : Object = this.value.asInstanceOf[Object]
     return (String.format("%3d -    ",res))
-  }
+ 
+}
 }
