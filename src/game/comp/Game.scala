3a1f48f509b39b6e405ef3a4f01589640807a802
Gabriella <gabriella.vg12@gmail.com>
2015-11-11 21:29:15 +0700

    Update Game.scala

--- src/game/comp/Game.scala
+++ src/game/comp/Game.scala
@@ -4,4 +4,101 @@ package game.comp
 
 class Game(val newp1:String, val newp2:String) {
 
+  private var p1 : Player = new Player(newp1)
+  private var p2 : Player = new Player(newp2)
+  private var board : Board = new Board()
+  private var dice : Dice = new Dice()
+  private var versusComputer : Boolean = false
+  private var gameOver : Boolean = false
+  
+  private var lastRoll : Int = _
+  private var curPlayer : Player = _
+
+  def isGameOver() : Boolean = {
+    return this.gameOver
+  }
+  
+  def Game(p1 : String) {
+    var p1 : Player = new Player(newp1)
+    var p2 : Player = new Player("Bot")
+    var board : Board = new Board()
+    var dice : Dice = new Dice()
+    var versusComputer : Boolean = true
+    var gameOver : Boolean = false
+  }
+
+  def start() : Unit = {
+    p1.setPosition(board.getTile(1))
+    p2.setPosition(board.getTile(1))
+    curPlayer = p1
+    printBoardString()
+  }
+ 
+  def play() : Unit = {
+    lastRoll = dice roll()
+    var position : Int = curPlayer.getPosition() + lastRoll
+    this.curPlayer.setPosition(board.getTile(nextPosition(position)))  
+    printBoardString()
+    end()
+  }
+  
+  private def end() : Unit = {
+    if (this.curPlayer.getPosition() == 100) {
+      this.gameOver = true
+      printWinner()
+    } 
+    else if (this.lastRoll != 6) {
+      if (this.curPlayer == this.p1) {
+        this.curPlayer = this.p2
+      } 
+      else {
+        this.curPlayer = this.p1
+      }
+    }
+  }
+
+  private def printWinner() : Unit = {
+        System.out.println("CONGRATULATION!!!")
+        System.out.println(this.curPlayer + " is the winner!")
+    }
+
+  def printBoardString() : Unit = {
+        var tiles : Array[Tile] = this.board.getBoard()
+        var r : String = new String()
+        for (i <- 0 to 9 by 1) {
+            var puluhan : Int = i * 10
+            if (i % 2 == 0) {  //genap
+                for (j <- 9 to 0 by -1) {
+                    r = " " + tiles(puluhan + j) + " |" + r
+                }
+            } 
+            else {  //ganjil
+                for (j <- 0 to 9 by 1) {
+                    r = " " + tiles(puluhan + j) + " |" + r
+                }
+            }
+            r = "\n|" + r
+        }
+        if (this.lastRoll != 0) {
+            r += "\nNow is " + this.curPlayer + "'s turn!"
+            r += "\nDice Roll " + this.lastRoll
+        }
+        r += "\n" + p1 + "'s position : " + p1.getPosition() + "\n"
+        r += p2 + "'s position : " + p2.getPosition()
+        System.out.println(r)
+    }
+
+    private def nextPosition(value : Int) : Int = {
+        var varVal : Int = value
+        if (isOverlap(varVal)) {
+            varVal = 200 - value
+        }
+        varVal = this.board.getTile(varVal).getTarget()
+        return varVal
+    }
+
+    private def isOverlap(value : Int) : Boolean = {
+        return value > 100
+    }
+
 }
