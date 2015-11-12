b2a066cb8ccb4301919e98258c9163ef0105c115
Gabriella <gabriella.vg12@gmail.com>
2015-11-12 12:19:37 +0700

    Update Main.scala

--- src/Main.scala
+++ src/Main.scala
@@ -6,10 +6,14 @@ import java.util.LinkedList
 
 object Main {
 
-  def main(args:Array[String]) {
-    
-   
+  def main(args:Array[String]) : Unit = {
+    var game : Game = new Game("Pemain")
+    game.start()
+    while(!game.isGameOver()){
+      game.play()
     }
   }
+  
+}
   
   
