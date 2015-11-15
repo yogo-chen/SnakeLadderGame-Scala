4ea173e7560f2440abe4d9ba0f892afaefdb261d
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-15 12:24:14 +0700

    Rearrange code

--- src/game/comp/TileNormal.scala
+++ src/game/comp/TileNormal.scala
@@ -1,12 +1,10 @@
 package game.comp
 
-class TileNormal( override val newValue:Int) extends Tile(newValue){
-this.target=this
-  
-  
-  override def toString(): String = {
-   val res : Object = this.value.asInstanceOf[Object]
-    return (String.format("%3d -    ",res))
- 
-}
+class TileNormal(override val newValue : Int) extends Tile(newValue) {
+	this.target = this
+
+	override def toString() : String = {
+		val res : Object = this.value.asInstanceOf[Object]
+		return (String.format("%3d -    ", res))
+	}
 }
