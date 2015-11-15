53cf0d703743f20f2b2593c7a835d977dfe35c08
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-15 22:20:31 +0700

    Update Player.scala

--- src/game/comp/Player.scala
+++ src/game/comp/Player.scala
@@ -1,20 +1,45 @@
 package game.comp
 
+/**
+ * Kelas Player yang merepresentasikan pemain di dalam SnakeLadder
+ * merupakan bagian dari kelas Model
+ * */
+ 
+ /**
+  * Constructor dari kelas Player
+  * param newname merupakan nama pemain
+  * */
 class Player(val newname : String) {
-	protected var name : String = newname
-	protected var position : Tile = _
+	protected var name : String = newname //nama pemain
+	protected var position : Tile = _//posisi pemain
 
+	/**
+	 * Method yang berfungsi untuk mengeset posisi pemain
+	 * */
 	def setPosition(tile : Tile) : Unit = {
 		this.position = tile
 	}
 
+	/**
+	 * Method yang berfungsi untuk mendapatkan posisi pemain
+	 * return posisi pemain
+	 * */
 	def getPosition() : Int = {
 		return this.position.getValue()
 	}
 
+	/**
+	 * Method yang berfungsi mendapatkan nama pemain
+	 * return nama pemain
+	 * */
 	def getName() : String = {
 		return this.name
 	}
+	
+	/**
+	 * Method yang berfungsi mendapatkan nama pemain
+	 * return nama pemain
+	 * */
 	override def toString() : String = {
 		return this.name
 	}
