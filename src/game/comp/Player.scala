6b053e13c5ebb58438550e74358e0f332810a761
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-15 12:20:54 +0700

    Rearrange code

--- src/game/comp/Player.scala
+++ src/game/comp/Player.scala
@@ -1,23 +1,21 @@
-
-
 package game.comp
 
-class Player(val newname:String) {
-  protected var name  : String = newname
-  protected var position :Tile=_
-  
-  def setPosition(tile:Tile):Unit ={
-    this.position=tile
-  }
-  
-  def getPosition():Int={
-   return this.position.getValue()
-}
-  
-  def getName():String={
-    return this.name
-  }
-  override def toString():String={
-    return this.name
-  }
+class Player(val newname : String) {
+	protected var name : String = newname
+	protected var position : Tile = _
+
+	def setPosition(tile : Tile) : Unit = {
+		this.position = tile
+	}
+
+	def getPosition() : Int = {
+		return this.position.getValue()
+	}
+
+	def getName() : String = {
+		return this.name
+	}
+	override def toString() : String = {
+		return this.name
+	}
 }
