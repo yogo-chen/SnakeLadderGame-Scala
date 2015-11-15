c450b65919cfb442c3c7cd7e5d309c8ba92815e1
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-15 12:12:54 +0700

    Rearrange code

--- src/game/comp/Dice.scala
+++ src/game/comp/Dice.scala
@@ -1,14 +1,11 @@
-
-
 package game.comp
 
 import scala.util.Random
+
 class Dice {
-  
-  private var r : Random = new Random()
-  
-  def roll():Int={
-    return r.nextInt(6)+1
-  }
-  
+	private var r : Random = new Random()
+
+	def roll() : Int = {
+		return r.nextInt(6) + 1
+	}
 }
