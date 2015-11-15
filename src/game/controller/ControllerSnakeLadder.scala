f0642122c4590650436cee0c0b5cc37767a64d99
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-15 22:02:13 +0700

    Update ControllerSnakeLadder.scala

--- src/game/controller/ControllerSnakeLadder.scala
+++ src/game/controller/ControllerSnakeLadder.scala
@@ -3,12 +3,17 @@
 package game.controller
 import game.view._
 import game.model._
+/**
+ * Kelas Kontroller yang menghubungkan object yang berfungsi sebagai view, dan kelas Game yang berfungsi sebagai model
+ **/
 
 class ControllerSnakeLadder (val option:Int ){
-  var opt : Int= option
+  var opt : Int= option //pilihan option yang digunakan dalam permainan
   
   
-  
+  /**
+   * method untuk mendapatkan hasil permainan per option, option 1 jika bermain dengan 2 player, option 2 jika bermain melawan komputer
+   **/
   def getOptionGame():Unit={
     if(opt==1){
       getVersusLawan()
