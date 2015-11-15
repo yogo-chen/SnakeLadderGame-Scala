e510a547cdcb2c985f4cf5510b54aea9b57d6ff0
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-15 22:08:11 +0700

    Update Dice.scala

--- src/game/comp/Dice.scala
+++ src/game/comp/Dice.scala
@@ -1,10 +1,17 @@
 package game.comp
 
 import scala.util.Random
-
+/**
+ * Kelas yang merepresentasikan dadu yang akan dikocok saat permainan
+ * Merupakan bagian dari kelas Model
+ * */
 class Dice {
 	private var r : Random = new Random()
 
+	/**
+	 * method pengocokan dadu, random dari nilai 1 sampai 6
+	 * return nilai dadu
+	 * */
 	def roll() : Int = {
 		return r.nextInt(6) + 1
 	}
