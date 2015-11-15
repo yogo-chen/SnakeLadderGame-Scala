34e5a17677efe4c66c64c938598d03fc972647e3
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-15 12:23:25 +0700

    Rearrange code

--- src/game/comp/TileLadder.scala
+++ src/game/comp/TileLadder.scala
@@ -1,17 +1,11 @@
-
-
 package game.comp
 
-class TileLadder(override val newValue: Int, newtarget:Tile) extends Tile(newValue ){
-    this.target  = newtarget
- 
+class TileLadder(override val newValue : Int, newtarget : Tile) extends Tile(newValue) {
+	this.target = newtarget
 
- 
-  override def toString() : String = {
-    
-  val resVal : Object = this.value.asInstanceOf[Object]
-  val resTarget  : Object =this.target.getValue().asInstanceOf[Object]
-   return (String.format("%3d - %-3d",resVal, resTarget))
-  
-}
+	override def toString() : String = {
+		val resVal : Object = this.value.asInstanceOf[Object]
+		val resTarget : Object = this.target.getValue().asInstanceOf[Object]
+		return (String.format("%3d - %-3d", resVal, resTarget))
+	}
 }
