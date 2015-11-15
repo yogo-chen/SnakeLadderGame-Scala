4734c1b930e6d1d2fdc1933cf744339b06d5dd90
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-15 12:25:08 +0700

    Rearrange code

--- src/game/comp/TileSnake.scala
+++ src/game/comp/TileSnake.scala
@@ -1,15 +1,11 @@
-
-
 package game.comp
 
-class TileSnake(override val newValue: Int, val newtarget:Tile) extends Tile (newValue){
-this.target=newtarget
+class TileSnake(override val newValue : Int, val newtarget : Tile) extends Tile (newValue) {
+	this.target = newtarget
 
-
-override def toString():String={
-  val resVal : Object = this.value.asInstanceOf[Object]
-  val resTarget  : Object =this.target.getValue().asInstanceOf[Object]
-  return  (String.format("%3d - %-3d", resVal,resTarget))
+	override def toString() : String = {
+		val resVal : Object = this.value.asInstanceOf[Object]
+		val resTarget : Object = this.target.getValue().asInstanceOf[Object]
+		return (String.format("%3d - %-3d", resVal, resTarget))
+	}
 }
-  
-} 
