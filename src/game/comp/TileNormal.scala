3558142bae76d1265f4268269fa76b9c8cd6f271
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-15 22:25:19 +0700

    Update TileNormal.scala

--- src/game/comp/TileNormal.scala
+++ src/game/comp/TileNormal.scala
@@ -1,8 +1,21 @@
 package game.comp
 
+/**
+ * Kelas TileNormal merepresentasikan tile tanpa ladder dan snake
+ * merupakan subclass dari kelas Tile
+ * merupakan bagian dari kelas Model
+ * */
+ 
+ /**
+  * Merupakan Constructor dari kelas TileNormal
+  * param newvalue merupakan posisi tile
+  * */
 class TileNormal(override val newValue : Int) extends Tile(newValue) {
 	this.target = this
 
+	/**
+	 * method toString yang menggambarkan posisi tile normal
+	 * */
 	override def toString() : String = {
 		val res : Object = this.value.asInstanceOf[Object]
 		return (String.format("%3d -    ", res))
