48809ee8a7253c2845c8eedda3252270cdc26483
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-15 22:26:17 +0700

    Update TileSnake.scala

--- src/game/comp/TileSnake.scala
+++ src/game/comp/TileSnake.scala
@@ -1,8 +1,23 @@
 package game.comp
 
+/**
+ * Merepresentasikan Tile yang memiliki ular
+ * merupakan subclass dari kelas Tile
+ * merupakan bagian dari kelas model
+ * */
+ 
+ 
+ /**
+  * Constructor TileSnake
+  * param newValue merupakan posisi dari tileladder, dan newtarget merupakan tempat ladder dituju
+  * */
 class TileSnake(override val newValue : Int, val newtarget : Tile) extends Tile (newValue) {
 	this.target = newtarget
 
+	
+	/**
+	 * method yang merepresentasikan posisi pemain awal dan akhir 
+	 * */
 	override def toString() : String = {
 		val resVal : Object = this.value.asInstanceOf[Object]
 		val resTarget : Object = this.target.getValue().asInstanceOf[Object]
