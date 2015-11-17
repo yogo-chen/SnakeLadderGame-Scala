79401211af337ea9d610037adc1f084f28d01dbf
Kresnadc <kresna_dcahyo@yahoo.co.id>
2015-11-17 21:38:41 +0700

    Update MainObject.scala
    Fix due to SnakeLadderGame.java newest version

--- src/game/view/MainObject.scala
+++ src/game/view/MainObject.scala
@@ -2,30 +2,67 @@
 
 package game.view
 
-import game.controller._
+import engine.Game
+import java.util.Arrays
+import java.util.LinkedList
 import java.util.Scanner
+import java.lang.Boolean
+import scala.util.control.Breaks._
 
-object MainObject {
-  
-  var sc : Scanner= new Scanner(System.in)
-  var opt : Int=_
-  
-  
-  def main(args:Array[String]) : Unit = {
-   println("ULAR TANGGA")
-   println()
-   println("PILIH LAWAN!")
-   println("1. LAWAN PLAYER")
-   println("2. LAWAN KOMPUTER")
-   println("Pilih")
-   opt = sc.nextInt()
-   getOpt()
-   
+object Main {
+  def main(args: Array[String]): Unit = {
+    this.start
   }
   
-  def getOpt():Unit={
-    var controller : ControllerSnakeLadder = new ControllerSnakeLadder(opt)
-    controller.getOptionGame()
-    
+  def start() {
+      val in = new Scanner(System.in)
+      var input : String = ""
+        do {
+            System.out.println("ULAR TANGGA")
+            System.out.println("1. Player versus Player")
+            System.out.println("2. Player versus Computer")
+            System.out.print("Option : ")
+            input = in.nextLine()
+        } while (!checkOption(input))
+
+        var game : Game = new Game()
+        if (Integer.parseInt(input) == 1) {
+            System.out.print("Player 1 name : ");
+            var p1 : String = in.next()
+            System.out.print("Player 2 name : ");
+            var p2 : String = in.next()
+            game = new Game(p1,p2) 
+        } else {
+            System.out.print("Player name : ");
+            var p1 : String = in.next()
+            game = new Game(p1) 
+        }
+
+        game.start()
+        in.nextLine()
+
+        while (!game.isGameOver()) {
+            System.out.print("\nNow is " + game.getCurPlayer() + "'s turn! Play [Y/n] ? ")
+            if (in.nextLine().trim().equalsIgnoreCase("n")) break
+            game.play();
+        }
   }
+
+  def checkOption(pilihan:String): Boolean ={
+        var res = true 
+        var value = 0
+        try {
+            value = Integer.parseInt(pilihan)
+            if (value != 1 && value != 2) {
+                res = false;
+            }
+        } catch {
+          case e: Exception => res = false
+        }
+        if (!res) {
+            System.out.println("You can only choose between 1 or 2!\n")
+        }
+        return res
+    }
+  
 }
