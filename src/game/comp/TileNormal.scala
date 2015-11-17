ce561bf55c6653eacec64ca2062429086fc04293
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-17 17:22:37 +0700

    Update TileNormal.scala

--- src/game/comp/TileNormal.scala
+++ src/game/comp/TileNormal.scala
@@ -12,12 +12,4 @@ package game.comp
   * */
 class TileNormal(override val newValue : Int) extends Tile(newValue) {
 	this.target = this
-
-	/**
-	 * method toString yang menggambarkan posisi tile normal
-	 * */
-	override def toString() : String = {
-		val res : Object = this.value.asInstanceOf[Object]
-		return (String.format("%3d -    ", res))
-	}
 }
