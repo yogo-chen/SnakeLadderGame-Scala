fee608407128c06ca6ef8cf82e961461bc389d28
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-17 17:12:32 +0700

    Update TileLadder.scala

--- src/game/comp/TileLadder.scala
+++ src/game/comp/TileLadder.scala
@@ -11,14 +11,5 @@ package game.comp
   * param newValue merupakan posisi dari tileladder, dan newtarget merupakan tempat ladder dituju
   * */
 class TileLadder(override val newValue : Int, newtarget : Tile) extends Tile(newValue) {
-	this.target = newtarget 
-
-	/**
-	 * method yang merepresentasikan posisi pemain awal dan akhir 
-	 * */
-	override def toString() : String = {
-		val resVal : Object = this.value.asInstanceOf[Object]
-		val resTarget : Object = this.target.getValue().asInstanceOf[Object]
-		return (String.format("%3d - %-3d", resVal, resTarget))
-	}
+	this.target = newtarget
 }
