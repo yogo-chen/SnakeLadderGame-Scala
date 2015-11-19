fdebded201aa5c6662473c281c35fd8b5c165cf5
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-19 20:11:46 +0700

    Update Game.scala

--- src/game/comp/Game.scala
+++ src/game/comp/Game.scala
@@ -3,122 +3,158 @@ package game.comp
 /**
  * Kelas Game yang merepresentasikan permainan Ular Tangga
  * Merupakan bagian dari kelas Model
- * */
- 
- /**
-  * Constructor pertama kelas Game, parameter 2 string untuk pemain pertama dan kedua
-  * */
-class Game(val newp1 : String, val newp2 : String) {
+ *
+ * Constructor pertama kelas Game, parameter 2 string untuk pemain pertama dan kedua
+ */
+class Game(val newp1: String, val newp2: String) {
+  private var p1: Player = new Player(newp1)
+  private var p2: Player = new Player(newp2)
+  private var curPlayer: Player = _
+  private var board: Board = new Board()
+  private var dice: Dice = new Dice()
 
-	private var p1 : Player = new Player(newp1)
-	private var p2 : Player = new Player(newp2)
-	private var board : Board = new Board()
-	private var dice : Dice = new Dice()
-	private var versusComputer : Boolean = false
-	private var gameOver : Boolean = false
+  private var versusComputer: Boolean = false
+  private var gameOver: Boolean = false
+  private var lastRoll: Int = _
+  private var firstPosition: Int = _
+  private var afterRollPosition: Int = _
+  private var finalPosition: Int = _
 
-	private var lastRoll : Int = _
-	private var curPlayer : Player = _
+  /**
+   * Constructor kedua dari Kelas Game, yang merepresentasikan permainan melawan komputer
+   * param 1 string merupakan nama pemain manusia
+   */
+  def this(newp1: String) {
+    this(newp1, "Bot")
+    this.versusComputer = true
+  }
 
-	/**
-	 * method untuk memeriksa apakah permainan telah berakhir
-	 * return true jika permainan berakhir, false sebaliknya
-	 */
-	def isGameOver() : Boolean = {
-		return this.gameOver
-	}
+  /**
+   * Method yang menunjukkan permainan dimulai, seluruh pemain diset menjadi berada di posisi pertama
+   */
+  def start() {
+    var zero = new Tile(0) {
+      override def toString(): String = return ""
+    }
+    this.p1.setPosition(zero)
+    this.p2.setPosition(zero)
+    this.curPlayer = this.p1
+    printBoardString()
+    printStatus()
+  }
 
-	/**
-	 *Constructor kedua dari Kelas Game, yang merepresentasikan permainan melawan komputer
-	 * param 1 string merupakan nama pemain manusia
-	 * */
-	def this(newp1 : String) {
-		this(newp1, "Bot")
-		var board : Board = new Board()
-		var dice : Dice = new Dice()
-		var versusComputer : Boolean = true
-		var gameOver : Boolean = false
-	}
+  /**
+   * method yang menjalankan permainan ular tangga
+   */
+  def play() {
+    if (this.versusComputer) {
+      playPVBot()
+    } else {
+      playPVP()
+    }
+  }
 
-	/**
-	 * Method yang menunjukkan permainan dimulai, seluruh pemain diset menjadi berada di posisi pertama
-	 * */
-	def start() : Unit = {
-		p1.setPosition(board.getTile(1))
-		p2.setPosition(board.getTile(1))
-		curPlayer = p1
-		printBoardString()
-	}
+  /**
+   * method yang mengembalikan player sekarang
+   */
+  def getCurPlayer(): Player = {
+    return this.curPlayer
+  }
 
-	/**
-	 * method yang menjalankan permainan ular tangga
-	 */
-	def play() : Unit = {
-		lastRoll = dice roll()
-		var position : Int = curPlayer.getPosition() + lastRoll
-		this.curPlayer.setPosition(board.getTile(nextPosition(position)))
-		printBoardString()
-		end()
-	}
+  /**
+   * method untuk memeriksa apakah permainan telah berakhir
+   * return true jika permainan berakhir, false sebaliknya
+   */
+  def isGameOver(): Boolean = {
+    return this.gameOver
+  }
 
-	private def end() : Unit = {
-		if (this.curPlayer.getPosition() == 100) {
-			this.gameOver = true
-			printWinner()
-		} 
-		else if (this.lastRoll != 6) {
-			if (this.curPlayer == this.p1) {
-				this.curPlayer = this.p2
-			} 
-			else {
-				this.curPlayer = this.p1
-			}
-		}
-	}
+  /**
+   * mencetak papan permainan ular tangga
+   */
+  def printBoardString() {
+    println(this.board)
+  }
 
-	private def printWinner() : Unit = {
-		System.out.println("CONGRATULATION!!!")
-		System.out.println(this.curPlayer + " is the winner!")
-	}
+  private def playPVP() {
+    updatePosition()
+    printBoardString()
+    printStatus()
+    endPlay()
+  }
 
-	/**		
-	 * mencetak papan permainan ular tangga		
-	 */
-	def printBoardString() : Unit = {
-		var tiles : Array[Tile] = this.board.getBoard()
-		var r : String = new String()
-		for (i <- 0 to 9 by 1) {
-			var puluhan : Int = i * 10
-			if (i % 2 == 0) {//genap
-				for (j <- 9 to 0 by -1) {
-					r = " " + tiles(puluhan + j) + " |" + r
-				}
-			} else {//ganjil
-				for (j <- 0 to 9 by 1) {
-					r = " " + tiles(puluhan + j) + " |" + r
-				}
-			}
-			r = "\n|" + r
-		}
-		if (this.lastRoll != 0) {
-			r += "\nNow is " + this.curPlayer + "'s turn!"
-			r += "\nDice Roll " + this.lastRoll
-		}
-		r += "\n" + p1 + "'s position : " + p1.getPosition() + "\n"
-		r += p2 + "'s position : " + p2.getPosition()
-		System.out.println(r)
-	}
+  private def playPVBot() {
+    printBoardString()
+    do {
+      updatePosition()
+      printStatus()
+      endPlay()
+    } while (this.curPlayer == this.p2 && !this.gameOver);
+  }
 
-	private def nextPosition(value : Int) : Int = {
-		var varVal : Int = value
-		if (isOverlap(varVal)) {
-			varVal = 200 - value
-		}
-		varVal = this.board.getTile(varVal).getTarget()
-		return varVal
-	}
+  private def endPlay() {
+    if (this.curPlayer.getPosition == 100) {
+      this.gameOver = true
+      printWinner()
+    } else if (this.lastRoll != 6) {
+      this.curPlayer = if (this.curPlayer == this.p1) this.p2 else this.p1
+    }
+  }
 
-	private def isOverlap(value : Int) : Boolean = {
-		return value > 100
-	}
+  private def printStatus() {
+    var r = new String()
+    if (this.lastRoll != 0) {
+      r += "\nDice roll " + this.lastRoll
+      r += "\n" + this.curPlayer + " move from " + this.firstPosition +
+        " to " +
+        this.afterRollPosition
+      if (this.board.getTile(this.afterRollPosition).isInstanceOf[TileLadder]) {
+        r += "\nYaay, " + this.curPlayer + " found a ladder to " +
+          this.finalPosition
+      } else if (this.board.getTile(this.afterRollPosition).isInstanceOf[TileSnake]) {
+        r += "\nAwww, " + this.curPlayer + " found a snake to " + this.finalPosition
+      }
+    }
+    r += "\n" + this.p1 + "'s position : " + this.p1.getPosition +
+      "\n"
+    r += this.p2 + "'s position : " + this.p2.getPosition
+    println(r)
+  }
+
+  private def printWinner() {
+    println("\nCONGRATULATION!!!")
+    println(this.curPlayer + " is the winner!")
+  }
+
+  private def updatePosition() {
+    roll()
+    setFirstPosition()
+    setAfterRollPosition()
+    setFinalPosition()
+    setCurPlayerPosition()
+  }
+
+  private def roll() {
+    this.lastRoll = this.dice.roll()
+  }
+
+  private def setFirstPosition() {
+    this.firstPosition = this.curPlayer.getPosition
+  }
+
+  private def setAfterRollPosition() {
+    var position = this.firstPosition + this.lastRoll
+    if (position > 100) {
+      position = 200 - position
+    }
+    this.afterRollPosition = position
+  }
+
+  private def setFinalPosition() {
+    this.finalPosition = this.board.getTile(this.afterRollPosition).getTarget
+  }
+
+  private def setCurPlayerPosition() {
+    this.curPlayer.setPosition(this.board.getTile(this.finalPosition))
+  }
 }
