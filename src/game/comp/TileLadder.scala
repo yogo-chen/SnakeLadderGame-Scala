3c0d9e9bf4b62ef9f8bad040002ae744077d42e8
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-19 20:13:33 +0700

    Update TileLadder.scala

--- src/game/comp/TileLadder.scala
+++ src/game/comp/TileLadder.scala
@@ -4,12 +4,12 @@ package game.comp
  * Merepresentasikan Tile yang memiliki tangga
  * merupakan subclass dari kelas Tile
  * merupakan bagian dari kelas model
- * */
- 
- /**
-  * Constructor TileLadder
-  * param newValue merupakan posisi dari tileladder, dan newtarget merupakan tempat ladder dituju
-  * */
-class TileLadder(override val newValue : Int, newtarget : Tile) extends Tile(newValue) {
-	this.target = newtarget
+ */
+
+/**
+ * Constructor TileLadder
+ * param newValue merupakan posisi dari tileladder, dan newtarget merupakan tempat ladder dituju
+ */
+class TileLadder(override val newValue: Int, newtarget: Tile) extends Tile(newValue) {
+  this.target = newtarget
 }
