93a3b77d63316149d71ff9e95b03f0a23af309df
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-19 20:12:59 +0700

    Update Tile.scala

--- src/game/comp/Tile.scala
+++ src/game/comp/Tile.scala
@@ -1,24 +1,24 @@
 package game.comp
 
-abstract class Tile(val newValue : Int) {
-	protected var value : Int = newValue
-	protected var target : Tile = this
+abstract class Tile(val newValue: Int) {
+  protected var value: Int = newValue
+  protected var target: Tile = this
 
-	def getValue() : Int = {
-		return this.value
-	}
+  def getValue(): Int = {
+    return this.value
+  }
 
-	def getTarget() : Int = {
-		return this.target.getValue()
-	}
+  def getTarget(): Int = {
+    return this.target.getValue()
+  }
 
-	override def toString() : String = {
-		val resVal : Object = this.value.asInstanceOf[Object]
-		val resTarget : Object = this.target.getValue().asInstanceOf[Object]
-		if(this.target == this){
-			return (String.format("%3d-   ", resVall))
-		} else {
-			return (String.format("%3d-%-3d", resVal, resTarget))
-		}
-	}
+  override def toString(): String = {
+    val resVal: Object = this.value.asInstanceOf[Object]
+    val resTarget: Object = this.target.getValue().asInstanceOf[Object]
+    if (this.target == this) {
+      return (String.format("%3d-   ", resVal))
+    } else {
+      return (String.format("%3d-%-3d", resVal, resTarget))
+    }
+  }
 }
