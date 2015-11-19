c1624c136ce7a64f8b8591db304d616dd68ff49f
Prayogo Cendra <prayogocendra@gmail.com>
2015-11-19 20:10:40 +0700

    Update Board.scala

--- src/game/comp/Board.scala
+++ src/game/comp/Board.scala
@@ -1,68 +1,81 @@
 package game.comp
 
 import scala.util.Random
+
 /**
  * Kelas Board yang merepresentaasikan papan dalam permainan SnakeLadder
  * merupakan bagian dari kelas model
- * */
+ */
 class Board {
-	var tiles : Array[Tile] = new Array[Tile](100)// kotak dalam 1 papan, terdiri dari 100 kotak
-	generateTiles(10,10)
+  private var tiles: Array[Tile] = new Array[Tile](100) // kotak dalam 1 papan, terdiri dari 100 kotak
+  generateTiles(10, 10)
 
-	/**
-	 * method untuk mendapatkan tile pada suatu posisi
-	 * return tile pada posisi tertentu
-	 * */
-	def getTile(value : Int) : Tile = {
-		return this.tiles(value-1)
-	}
+  /**
+   * method untuk mendapatkan tile pada suatu posisi
+   * return tile pada posisi tertentu
+   */
+  def getTile(value: Int): Tile = {
+    return this.tiles(value - 1)
+  }
 
-	/**
-	 * method untuk mendapatkan papan
-	 * return papan 
-	 * */
-	def getBoard() : Array[Tile] = {
-		return this.tiles
-	}
+  override def toString(): String = {
+    var r = new String()
+    for (i <- 0 until 10) {
+      val puluhan = i * 10
+      if (i % 2 == 0) {
+        var j = 9
+        while (j >= 0) {
+          r = this.tiles(puluhan + j) + "|" + r
+          j -= 1
+        }
+      } else {
+        for (j <- 0 until 10) {
+          r = this.tiles(puluhan + j) + "|" + r
+        }
+      }
+      r = "\n|" + r
+    }
+    r
+  }
 
-	private def generateTiles(snake : Int, ladder : Int) : Unit = {
-		var r : Random = new Random()
-		generateNormalTiles()
-		generateSnakeTiles(r, snake)
-		generateLadderTiles(r, ladder)
-	}
+  private def generateTiles(snake: Int, ladder: Int) {
+    var r: Random = new Random()
+    generateNormalTiles()
+    generateSnakeTiles(r, snake)
+    generateLadderTiles(r, ladder)
+  }
 
-	private def generateNormalTiles() : Unit = {
-		for(i<-this.tiles.length-1 to 0 by -1) {
-			this.tiles(i) = new TileNormal(i + 1)
-		}
-	}
+  private def generateNormalTiles() {
+    for (i <- this.tiles.length - 1 to 0 by -1) {
+      this.tiles(i) = new TileNormal(i + 1)
+    }
+  }
 
-	private def generateSnakeTiles(r : Random, snake : Int) : Unit = {
-		var initial : Int = 0
-		var target : Int = 0
-		for(i<-snake to 0 by -1 ) {
-			do {
-				initial = r.nextInt(79) + 20
-			} while ((!(this.tiles(initial).isInstanceOf[TileNormal])))
-			do {
-				target = r.nextInt(initial-10) + 1
-			} while (!(this.tiles(target).isInstanceOf[TileNormal]))
-			this.tiles(initial) = new TileSnake(initial + 1, this.tiles(target))
-		}
-	}
+  private def generateSnakeTiles(r: Random, snake: Int) {
+    var initial: Int = 0
+    var target: Int = 0
+    for (i <- snake to 0 by -1) {
+      do {
+        initial = r.nextInt(79) + 20
+      } while ((!(this.tiles(initial).isInstanceOf[TileNormal])))
+      do {
+        target = r.nextInt(initial - 10) + 1
+      } while (!(this.tiles(target).isInstanceOf[TileNormal]))
+      this.tiles(initial) = new TileSnake(initial + 1, this.tiles(target))
+    }
+  }
 
-	private def generateLadderTiles(r : Random, ladder : Int) : Unit = {
-		var initial : Int = 0
-		var target : Int = 0
-		for(i<-ladder to 0 by -1) {
-			do {
-				initial = r.nextInt(79) + 1
-			} while (!(this.tiles(initial).isInstanceOf[TileNormal]))
-			do {
-				target = r.nextInt(99-initial) + initial
-			} while(!(this.tiles(target).isInstanceOf[TileNormal]))
-			this.tiles(initial) = new TileLadder(initial + 1, this.tiles(target)) 
-		}
-	}
+  private def generateLadderTiles(r: Random, ladder: Int) {
+    var initial: Int = 0
+    var target: Int = 0
+    for (i <- ladder to 0 by -1) {
+      do {
+        initial = r.nextInt(79) + 1
+      } while (!(this.tiles(initial).isInstanceOf[TileNormal]))
+      do {
+        target = r.nextInt(99 - initial) + initial
+      } while (!(this.tiles(target).isInstanceOf[TileNormal]))
+      this.tiles(initial) = new TileLadder(initial + 1, this.tiles(target))
+    }
+  }
 }
