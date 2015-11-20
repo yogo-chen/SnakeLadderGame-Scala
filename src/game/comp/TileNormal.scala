7e863a4021d0f517975fa126335e957124623713
Kresnadc <kresna_dcahyo@yahoo.co.id>
2015-11-20 13:41:19 +0700

    Update TileNormal.scala

--- src/game/comp/TileNormal.scala
+++ src/game/comp/TileNormal.scala
@@ -4,11 +4,9 @@ package game.comp
  * Kelas TileNormal merepresentasikan tile tanpa ladder dan snake
  * merupakan subclass dari kelas Tile
  * merupakan bagian dari kelas Model
- */
-
-/**
- * Merupakan Constructor dari kelas TileNormal
- * param newvalue merupakan posisi tile
+ * 
+ * @constructor Merupakan Constructor dari kelas TileNormal
+ * @param newvalue merupakan posisi tile
  */
 class TileNormal(override val newValue: Int) extends Tile(newValue) {
   this.target = this
