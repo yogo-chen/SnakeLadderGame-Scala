f0010d56c96cdc609f661a04cca43c942a8b515d
MelindaNurAbianti <melindanurabianti@gmail.com>
2015-11-20 13:48:31 +0700

    Update Main.scala

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,7 @@
 import game.SnakeLadderTextMode
-
+/**
+ * Kelas yang merepresentasikan Tester permainan Snake and Ladder
+ */
 object Main {
   def main(args: Array[String]) {
     val game = new SnakeLadderTextMode()
