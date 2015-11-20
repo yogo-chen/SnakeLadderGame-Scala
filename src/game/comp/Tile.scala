4e6616edeb3f7c80e63909a8dc806fed4204dce7
Kresnadc <kresna_dcahyo@yahoo.co.id>
2015-11-20 13:39:35 +0700

    Update Tile.scala

--- src/game/comp/Tile.scala
+++ src/game/comp/Tile.scala
@@ -1,17 +1,34 @@
 package game.comp
 
+/**
+ * Kelas Tile mempresentasikan kotak pada Board permainan
+ * @constructor membuat sebuah tile baru dengan nomor posisi
+ * @param newValue angka untuk menentukan nomor posisi
+ */
 abstract class Tile(val newValue: Int) {
   protected var value: Int = newValue
   protected var target: Tile = this
-
+  
+  /**
+   * method getter dari atribut value
+   * @return nomor posisi
+   */
   def getValue(): Int = {
     return this.value
   }
 
+  /**
+   * method getter dari atribut target
+   * @return nomor posisi target
+   */
   def getTarget(): Int = {
     return this.target.getValue()
   }
-
+  
+  /**
+   * method yang meng-Override toString
+   * @return String dari atribut kelas Tile
+   */
   override def toString(): String = {
     val resVal: Object = this.value.asInstanceOf[Object]
     val resTarget: Object = this.target.getValue().asInstanceOf[Object]
